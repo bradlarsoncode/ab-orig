ef158db30b1473ab8af674a43f6740473363ff26
Brad Larson <larsonbrad@pm.me>
2021-05-07 09:29:15 -0400

    cal

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -1,5 +1,6 @@
 import { DateRangePicker } from "react-date-range";
 import React, { useState } from "react";
+import {Link} from 'react-router-dom'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { addDays } from "date-fns";
@@ -54,20 +55,22 @@ console.log(startDate, endDate)
           <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
             <div class="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg">
               <p class="font-semibold text-gray-800">Book Your Stay</p>
-              <svg
-                class="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+              <Link to="/">
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              </Link>
             </div>
             <div class="flex flex-col px-3 py-5 bg-gray-50">
               <div className="flex justify-center pt-4">
